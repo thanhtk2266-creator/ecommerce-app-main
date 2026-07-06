583c0779b2e834ce8b0c7103918e5d3954086981
Kim thanh <thanhtk2266@ut.edu.vn>
2026-07-06 13:17:33 +0700

    test deloy

--- frontend/src/utils/api/variables.js
+++ frontend/src/utils/api/variables.js
@@ -1,9 +1,9 @@
 export const variables = {
-    BASE_URL: "http://3.26.181.129:5000/api/",
+    BASE_URL: "http://13.239.147.88:5000/api/",
 
-    USER_API: "http://3.26.181.129:5000/api/user",
-    PRODUCT_API: "http://3.26.181.129:5000/api/product",
-    ORDER_API: "http://3.26.181.129:5000/api/order",
-    PRODUCTSIZE_API: "http://3.26.181.129:5000/api/productsize",
-    ORDERITEM_API: "http://3.26.181.129:5000/api/orderi123tem"
+    USER_API: "http://13.239.147.88:5000/api/user",
+    PRODUCT_API: "http://13.239.147.88:5000/api/product",
+    ORDER_API: "http://13.239.147.88:5000/api/order",
+    PRODUCTSIZE_API: "http://13.239.147.88:5000/api/productsize",
+    ORDERITEM_API: "http://13.239.147.88:5000/api/orderi123tem"
 };
